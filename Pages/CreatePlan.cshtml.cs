795d35737e55d0cb85d85f20004981e38d4f1a9f
oidex69 <shresthaprince222@gmail.com>
2026-05-27 16:22:13 +0545

    fixes in production and created createplan page

---
+++ Pages/CreatePlan.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace kaam.Pages;
+
+public class CreatePlan : PageModel
+{
+  public void OnGet()
+  {
+  }
+}
---
+++ Pages/Production.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace kaam.Pages;
+
+public class Production : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
